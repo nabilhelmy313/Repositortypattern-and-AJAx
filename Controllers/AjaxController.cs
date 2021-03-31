ea8cefc5af1772597a863d7e3ca5af520f69cd17
nabil <nabilhelmy313@gmail.com>
2021-04-01 00:43:31 +0200

    Ajax

---
+++ Controllers/AjaxController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Repo.Models.Data;
+using Repo.Models.Repos;
+using Repo.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repo.Controllers
+{
+    public class AjaxController : Controller
+    {
+        private readonly IRepoProj<Country> country;
+        private readonly IRepoProj<City> city;
+
+        public AjaxController(IRepoProj<Country> country,IRepoProj<City> city)
+        {
+            this.country = country;
+            this.city = city;
+        }
+        public IActionResult Index()
+        {
+            CountryCityVM vM = new CountryCityVM
+            {
+                Countries = country.List(),
+            };
+            return View(vM);
+        }
+        public JsonResult GetCities(int CountryId)
+        {
+            var c = country.Find(CountryId);
+            var cit = city.ListByFilter(cc => cc.Country == c);
+            return Json(new SelectList(cit,"Id","Name"));
+        }
+    }
+}
---
+++ ViewModel/CountryCityVM.cs
@@ -0,0 +1,15 @@
+﻿using Repo.Models.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Repo.ViewModel
+{
+    public class CountryCityVM
+    {
+        public List<Country> Countries{ get; set; }
+        public int CountryId { get; set; }
+
+    }
+}
